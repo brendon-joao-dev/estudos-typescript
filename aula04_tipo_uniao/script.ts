8bbb65199060ace3b6c2808f3ea24691c6197e8e
brendon-joao-dev <brendonjoaodev@gmail.com>
2026-04-17 23:02:36 -0300

    Aula04: Tipo união do Typescript para variáveis com mais de um tipo

---
+++ aula04_tipo_uniao/script.ts
@@ -0,0 +1,12 @@
+// Lê-se: União recebe uma string ou um número
+let uniao: string | number;
+
+uniao = 5;
+console.log(uniao);
+
+uniao = "Brendon";
+console.log(uniao);
+
+// Novamente roda mas reclama
+// uniao = true;
+// console.log(uniao);
